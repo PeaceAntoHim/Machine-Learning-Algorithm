731f5ad0a5451d70432ad3c814adc9048ff1d459
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-05-04 16:27:26 +0700

    learn how to program R data processing

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/categorical_data.R
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/R/categorical_data.R
@@ -4,14 +4,17 @@
 dataset = read.csv('Data.csv')
 
 # Taking care of missing data
+# This to take care of missing data Age and fill it with mean calculation of data
 dataset$Age = ifelse(is.na(dataset$Age),
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Age)
+# This to take care of missing data Salery and fill it with mean calculation of data
 dataset$Salary = ifelse(is.na(dataset$Salary),
                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                         dataset$Salary)
 
 # Encoding categorical data
+# This to change all data into binary and will easy mhen machine learning process
 dataset$Country = factor(dataset$Country,
                          levels = c('France', 'Spain', 'Germany'),
                          labels = c(1, 2, 3))
